e5bfd975deb259db6ca4d3cc00db94c938606eea
Vyacheslav <grinksic@gmail.com>
2022-09-16 23:24:08 +0300

    GetShort Method is added

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -14,3 +14,23 @@ string[] arr2 = { "1234", "1567", "-2", "computer science" };
 string[] arr3 = { "Russia", "Denmark", "Kazan" };
 
 int maxLetters = 3;
+
+string[] GetShort (string[] array)
+		{
+			int length = array.Length;			
+			string[] result = new string[length];
+			int count = 0;
+			
+			for (int i = 0; i < length; i++)
+			{
+				if(array[i].Length <= maxLetters)
+				{
+					result[count] = array[i];
+					count++;
+				}
+			}
+			Array.Resize(ref result, count);
+			
+			return result;
+		}
+		
