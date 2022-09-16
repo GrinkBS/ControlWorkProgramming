292fcbb36c9ea4e885ac2d3ed974a9eca67dcc5e
Vyacheslav <grinksic@gmail.com>
2022-09-16 23:21:15 +0300

    strins are added

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -9,3 +9,8 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+string[] arr1 = { "Hello", "2", "world", ":-)" };
+string[] arr2 = { "1234", "1567", "-2", "computer science" };
+string[] arr3 = { "Russia", "Denmark", "Kazan" };
+
+int maxLetters = 3;
